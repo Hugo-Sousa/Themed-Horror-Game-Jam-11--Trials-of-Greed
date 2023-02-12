570f43ea575d66002c17cf200890ec3562902646
HugoSousa-38 <a46304@alunos.isel.pt>
2023-02-12 13:20:39 +0000

    Base Update

---
+++ SpookCrawler/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public float speed;
+    public Rigidbody2D rb;
+    private Vector2 movement;
+    
+    public Animator anim;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Vertical", movement.y);
+    }
+    
+    private void FixedUpdate()
+    {
+        Controller();
+    }
+    
+    private void Controller()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    public void ChangeScale(float scale)
+    {
+        transform.localScale = new Vector3(scale, scale, 1);
+    }
+}
